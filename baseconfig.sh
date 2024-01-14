f5669f394903a415f302f70628a93098dbd1ffd4
Styx-lab51 <153520752+Styx-lab51@users.noreply.github.com>
2024-01-14 15:00:51 +0100

    Update baseconfig.sh

--- baseconfig.sh
+++ baseconfig.sh
@@ -1,1 +1,52 @@
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo swapoff -a
 
+echo '192.168.0.210	node01' >> /etc/hosts
+echo '192.168.0.220	node02' >> /etc/hosts
+echo '192.168.0.230	master' >> /etc/hosts
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee test.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+curl https://get.docker.com -o install_docker.sh
+sudo chmod +x install_docker.sh
+./install_docker.sh
+
+sudo systemctl daemon-reload
+sudo systemctl enable docker --now
+sudo systemctl status docker
+
+sudo rm -f /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+sudo systemctl status containerd
+
+sudo sed -i '/^ExecStart/ s/$/ --exec-opt native.cgroupdriver=systemd/' /usr/lib/systemd/system/docker.service
+
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+sudo systemctl status kubelet
